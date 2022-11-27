248e27d7132e972f2f5fc7caeb773617cc343cee
Adam Taylor <adamt@wavecable.com>
2022-11-27 15:16:11 -0800

    uncommented lint error validator

--- project_web_page/test.spec.js
+++ project_web_page/test.spec.js
@@ -69,38 +69,38 @@ describe('HTML content checks', () => {
 
 })
 
-// describe('Source code is valid', () => {
-//   test('HTML validates without errors', async () => {
-//     const lintOpts = {
-//       'attr-bans': ['align', 'background', 'bgcolor', 'border', 'frameborder', 'marginwidth', 'marginheight', 'scrolling', 'style', 'width', 'height'], // adding height, allow longdesc
-//       'tag-bans': ['style', 'b'], // <i> allowed for font-awesome
-//       'doctype-first': true,
-//       'doctype-html5': true,
-//       'html-req-lang': true,
-//       'attr-name-style': false, // for meta tags
-//       'line-end-style': false, // either way
-//       'indent-style': false, // can mix/match
-//       'indent-width': false, // don't need to beautify
-//       'id-class-style': false, // I like dashes in classnames
-//       'img-req-alt': true
-//     }
+describe('Source code is valid', () => {
+  test('HTML validates without errors', async () => {
+    const lintOpts = {
+      'attr-bans': ['align', 'background', 'bgcolor', 'border', 'frameborder', 'marginwidth', 'marginheight', 'scrolling', 'style', 'width', 'height'], // adding height, allow longdesc
+      'tag-bans': ['style', 'b'], // <i> allowed for font-awesome
+      'doctype-first': true,
+      'doctype-html5': true,
+      'html-req-lang': true,
+      'attr-name-style': false, // for meta tags
+      'line-end-style': false, // either way
+      'indent-style': false, // can mix/match
+      'indent-width': false, // don't need to beautify
+      'id-class-style': false, // I like dashes in classnames
+      'img-req-alt': true
+    }
 
-//     for (const f of html_files) {
-//       await expect(__dirname + '/' + f).toHaveNoHtmlLintErrorsAsync(lintOpts)
-//     }
-//   })
+    for (const f of html_files) {
+      await expect(__dirname + '/' + f).toHaveNoHtmlLintErrorsAsync(lintOpts)
+    }
+  })
 
-//   test('CSS validates without errors', async () => {
-//     await expect(__dirname +'/css/*.css').toHaveNoCssLintErrorsAsync() // test all files in css folder
-//   })
+  test('CSS validates without errors', async () => {
+    await expect(__dirname +'/css/*.css').toHaveNoCssLintErrorsAsync() // test all files in css folder
+  })
 
-//   test('JavaScript lints without errors', () => {
-//     if (fs.existsSync(path.join(__dirname, 'js'))) {
+  test('JavaScript lints without errors', () => {
+    if (fs.existsSync(path.join(__dirname, 'js'))) {
       
 
-//       for (const f of js_files) {
-//         expect([__dirname + '/js/' + f]).toHaveNoEsLintErrors()
-//       }
-//     }
-//   })
-// })
+      for (const f of js_files) {
+        expect([__dirname + '/js/' + f]).toHaveNoEsLintErrors()
+      }
+    }
+  })
+})
